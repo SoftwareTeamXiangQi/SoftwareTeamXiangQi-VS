8d19d8df3bce86dd8b8cb56f4b26f1a1a5de70d7
Shen-Ziman <765353219@qq.com>
2022-01-14 22:24:56 +0800

    添加项目文件。

---
+++ System/Envionment.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    internal class Envionment
+    {
+        internal static void Exit(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
